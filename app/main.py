870b4cbe6087e7043fb3cf482a323a4a4b7fb8a0
farizarvin <farizarvin@gmail.com>
2025-09-18 21:30:03 +0700

    add new ip

--- app/main.py
+++ app/main.py
@@ -81,6 +81,7 @@ app.add_middleware(
         "http://localhost:3000", 
         "http://127.0.0.1:3000",
         "http://localhost:3001",
+        "http://52.87.172.176:8000",
         "*"  # For development only
     ],
     allow_credentials=True,
